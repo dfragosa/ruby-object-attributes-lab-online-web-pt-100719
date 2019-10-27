e72d58fc7a385e01137356abd6579875a032770d
Derric Fragosa <fderric@gmail.com>
2019-10-27 20:37:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@ class Dog
   # def initialize name
   # @name = name 
   # end
-   def breed=(breed)
-  @breed= breed
+   def name=(name)
+  @name= name
   end
   
   def name
-    @breed
+    @name
   end 
   
 end 
